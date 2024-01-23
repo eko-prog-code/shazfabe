4d77abd31adde314cae0e5fbb3cc5db629c5038a
ruth-code-prog <ekosetiaji929@gmail.com>
2024-01-24 02:39:54 +0700

    next

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5000;
