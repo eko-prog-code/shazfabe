6283825aa35db847f69b915114af6edf2c946798
ruth-code-prog <ekosetiaji929@gmail.com>
2024-01-27 00:44:35 +0700

    Obs-Nadi

--- server.js
+++ server.js
@@ -76,6 +76,36 @@ app.post('/forward-request-condition', async (req, res) => {
     }
 });
 
+app.post('/forward-request-observation-nadi', async (req, res) => {
+    try {
+        // Ambil token dari Firebase Realtime Database
+        const firebaseTokenUrl = "https://rme-shazfa-mounira-default-rtdb.firebaseio.com/token.json";
+        const response = await axios.get(firebaseTokenUrl);
+        const accessToken = response.data.token; // Sesuaikan dengan struktur data Anda
+
+        const apiUrl = 'https://api-satusehat-dev.dto.kemkes.go.id/fhir-r4/v1/Observation';
+
+        // Log the curl command to the console
+        console.log(`curl -X POST "${apiUrl}" -H "Content-Type: application/json" -H "Authorization: Bearer ${accessToken}" -d '${JSON.stringify(req.body)}'`);
+
+        const { default: fetch } = await import('node-fetch');
+        const apiResponse = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(req.body),
+        });
+
+        const responseData = await apiResponse.json();
+        res.json(responseData);
+    } catch (error) {
+        console.error('Error forwarding request:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 const getIHSParticipant = async (identifier, accessToken) => {
     try {
         const firebaseTokenUrl = "https://rme-shazfa-mounira-default-rtdb.firebaseio.com/token.json";
